a66a511a6e952962e701311b55ecc818d2b16d79
Curlykonda <bartsch.henning@gmail.com>
2019-05-16 15:40:57 +0200

    Implemented VAE

--- assignment_3/code/a3_vae_template.py
+++ assignment_3/code/a3_vae_template.py
@@ -1,4 +1,6 @@
 import argparse
+import numpy as np
+from pathlib import Path
 
 import torch
 import torch.nn as nn
@@ -10,8 +12,26 @@ from datasets.bmnist import bmnist
 
 class Encoder(nn.Module):
 
-    def __init__(self, hidden_dim=500, z_dim=20):
+    def __init__(self, hidden_dim=500, z_dim=20, in_dim=784):
         super().__init__()
+
+        #Architecture according to Kingma & Welling
+        #tanh were replaced by ReLU
+        self.fc1 = nn.Linear(in_dim, hidden_dim)
+
+        self.mean_mlp = nn.Sequential(
+            self.fc1,
+            nn.ReLU(),
+            nn.Linear(hidden_dim, z_dim)
+        )
+
+
+        self.sigma_mlp = nn.Sequential(
+            self.fc1,
+            nn.ReLU(),
+            nn.Linear(hidden_dim, z_dim),
+            nn.Softplus() #assures that std is non-negative
+        )
 
     def forward(self, input):
         """
@@ -20,16 +40,26 @@ class Encoder(nn.Module):
         Returns mean and std with shape [batch_size, z_dim]. Make sure
         that any constraints are enforced.
         """
-        mean, std = None, None
-        raise NotImplementedError()
+        #mean, std = None, None
+        #raise NotImplementedError()
+
+        mean = self.mean_mlp(input)
+        std = self.sigma_mlp(input)
 
         return mean, std
 
 
 class Decoder(nn.Module):
 
-    def __init__(self, hidden_dim=500, z_dim=20):
+    def __init__(self, hidden_dim=500, z_dim=20, out_dim=784):
         super().__init__()
+
+        self.decode_mlp = nn.Sequential(
+            nn.Linear(z_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, out_dim),
+            nn.Sigmoid()
+        )
 
     def forward(self, input):
         """
@@ -37,28 +67,60 @@ class Decoder(nn.Module):
 
         Returns mean with shape [batch_size, 784].
         """
-        mean = None
-        raise NotImplementedError()
+        mean = self.decode_mlp(input)
 
         return mean
 
 
 class VAE(nn.Module):
 
-    def __init__(self, hidden_dim=500, z_dim=20):
+    def __init__(self, hidden_dim=500, z_dim=20, data_dim=784, device='cpu'):
         super().__init__()
 
         self.z_dim = z_dim
+        self.data_dim = data_dim
+        self.eps = 1e-8 #computational stability
+        self.device = device
         self.encoder = Encoder(hidden_dim, z_dim)
         self.decoder = Decoder(hidden_dim, z_dim)
+
+    def compute_elbo(self, input, recon, mu, std):
+
+        std = std + self.eps
+        #recon = recon + self.eps
+
+        #reconstruction loss: neg log likelihood Bernoulli
+        #See Kingma & Welling Appendix C1
+        recon_loss = -1 * torch.sum(input * torch.log(recon) + (1-input) * torch.log(1 - recon), dim=1)
+        #BCE = F.binary_cross_entropy(recon, input, reduction='sum')
+
+        #regularisation loss: KL-divergence approx posterior and prior, Gaussian case
+        #See Kingma & Welling Appendix B
+        var = std.pow(2)
+        reg_loss = 0.5 * torch.sum(1 + torch.log(var) - mu.pow(2) - var)
+
+        return torch.mean(recon_loss + reg_loss, dim=0)
 
     def forward(self, input):
         """
         Given input, perform an encoding and decoding step and return the
         negative average elbo for the given batch.
         """
-        average_negative_elbo = None
-        raise NotImplementedError()
+
+        #encode
+        mu, std = self.encoder(input)
+
+        #reparameterise
+        epsilon = torch.randn_like(std).to(self.device)
+        z = mu + std * epsilon
+        z.to(self.device)
+
+        #decode
+        recon = self.decoder(z)
+
+        #compute elbo
+        average_negative_elbo = self.compute_elbo(input, recon, mu, std)
+
         return average_negative_elbo
 
     def sample(self, n_samples):
@@ -67,8 +129,20 @@ class VAE(nn.Module):
         (from bernoulli) and the means for these bernoullis (as these are
         used to plot the data manifold).
         """
-        sampled_ims, im_means = None, None
-        raise NotImplementedError()
+        #sample from unit Gaussian
+        z = torch.randn(n_samples, self.z_dim).to(self.device)
+
+        #decode latent vectors
+        sampled_ims = self.decoder(z)
+        print(f"Shape sampled ims:{sampled_ims.shape}")
+
+        #compute means
+        im_means = sampled_ims.mean(dim=0)
+
+        #reshape samples
+        im_dim = int(np.sqrt(self.data_dim))
+        sampled_ims = sampled_ims.view(-1,1,im_dim,im_dim)
+        im_means = im_means.view(1,1,im_dim,im_dim)
 
         return sampled_ims, im_means
 
@@ -80,10 +154,24 @@ def epoch_iter(model, data, optimizer):
 
     Returns the average elbo for the complete epoch.
     """
-    average_epoch_elbo = None
-    raise NotImplementedError()
+    average_epoch_elbo = 0
 
-    return average_epoch_elbo
+    for i, mb in enumerate(data):
+
+        #reshape mini-batch
+        mb = mb.reshape(-1, model.data_dim).to(model.device)
+        #forward pass
+        elbo = model(mb)
+
+        #Perform backward pass in training mode
+        if model.training:
+            optimizer.zero_grad()
+            elbo.backward()
+            optimizer.step()
+
+    average_epoch_elbo += elbo.item()
+
+    return average_epoch_elbo / i
 
 
 def run_epoch(model, data, optimizer):
@@ -111,14 +199,41 @@ def save_elbo_plot(train_curve, val_curve, filename):
     plt.tight_layout()
     plt.savefig(filename)
 
+def save_vae_samples(model, epoch, path, n_row=8):
+
+    with torch.no_grad():
+        sample_ims, im_means = model.sample(n_row**2)
+        #transform samples to grid represenation
+        sample_ims = make_grid(sample_ims, nrow=n_row)
+
+        #format sampled images
+        samples = sample_ims.cpu().numpy().transpose(1,2,0)
+
+        #save samples
+        file_name = (f"sample_{epoch}.png")
+        plt.imsave(path / file_name, samples)
+        print(f"Saved {file_name}\n")
 
 def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     data = bmnist()[:2]  # ignore test split
-    model = VAE(z_dim=ARGS.zdim)
+
+    #initialise VAE model
+    model = VAE(z_dim=ARGS.zdim, device=device)
+    model.to(device)
+
     optimizer = torch.optim.Adam(model.parameters())
 
+    # make the results directory
+    res_path = Path.cwd() / 'out_vae'
+    res_path.mkdir(parents=True, exist_ok=True)
+
+    #sample from VAE
+    save_vae_samples(model, epoch=0, path=res_path)
+
     train_curve, val_curve = [], []
-    for epoch in range(ARGS.epochs):
+    for epoch in range(1, ARGS.epochs+1):
         elbos = run_epoch(model, data, optimizer)
         train_elbo, val_elbo = elbos
         train_curve.append(train_elbo)
@@ -127,13 +242,12 @@ def main():
 
         # --------------------------------------------------------------------
         #  Add functionality to plot samples from model during training.
-        #  You can use the make_grid functioanlity that is already imported.
         # --------------------------------------------------------------------
+        save_vae_samples(epoch, res_path)
 
     # --------------------------------------------------------------------
     #  Add functionality to plot plot the learned data manifold after
-    #  if required (i.e., if zdim == 2). You can use the make_grid
-    #  functionality that is already imported.
+    #  if required (i.e., if zdim == 2).
     # --------------------------------------------------------------------
 
     save_elbo_plot(train_curve, val_curve, 'elbo.pdf')
